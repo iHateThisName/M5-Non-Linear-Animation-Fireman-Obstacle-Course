2ac4c4bbac81d11b84cc0f32d51d1df8b504d130
iHateThisName <ivarravi0@gmail.com>
2026-05-10 19:20:40 +0200

    Feature Added, Switching between cameras

--- Assets/_Scripts/Managers/CameraManager.cs
+++ Assets/_Scripts/Managers/CameraManager.cs
@@ -1,11 +1,11 @@
 using Assets.Scripts.Singleton;
-using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraManager : Singleton<CameraManager> {
-    [SerializeField] private List<CinemachineVirtualCameraBase> cinemachineCameras;
+    [SerializeField] private CinemachineVirtualCameraBase aimCamera;
+    [SerializeField] private CinemachineVirtualCameraBase followCamera;
     [SerializeField] private InputActionReference aimRefrence;
     private InputAction aimAction;
 
@@ -27,8 +27,21 @@ public class CameraManager : Singleton<CameraManager> {
     private void OnAim(InputAction.CallbackContext context) {
         if (context.performed) {
             this.isAiming = true;
+            ToggleCameraPriorty();
+
         } else if (context.canceled) {
             this.isAiming = false;
+            ToggleCameraPriorty();
+        }
+    }
+
+    private void ToggleCameraPriorty() {
+        if (this.isAiming) {
+            this.aimCamera.Priority = 1;
+            this.followCamera.Priority = 0;
+        } else {
+            this.aimCamera.Priority = 0;
+            this.followCamera.Priority = 1;
         }
     }
 }
